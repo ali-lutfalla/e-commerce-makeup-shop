2a3a329076e5883ac4deab0416d44278b6356db9
ali-lutfalla <ali,latfalla@gmail.com>
2024-09-06 13:15:11 +0300

    created and added the database connection

--- e_commerce_project/settings.py
+++ e_commerce_project/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'e_commerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'e-shopDB',
     }
 }
 
