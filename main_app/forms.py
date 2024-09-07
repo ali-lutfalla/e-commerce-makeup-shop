9242a16f953eb5cee67a381660813ebd0325bdaa
ali-lutfalla <ali,latfalla@gmail.com>
2024-09-07 20:32:32 +0300

    fixed the problems of accounts with the same email or username

--- main_app/forms.py
+++ main_app/forms.py
@@ -12,6 +12,18 @@ class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with that email already exists.")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("A user with that username already exists.")
+        return username
 
 class UserSignInForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
